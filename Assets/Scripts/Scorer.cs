8e777679641980eb130e19c44eacf23d42af1c98
Javier Meléndez <javier.melendez@fyware.com>
2022-03-05 21:17:11 -0600

    [feature] Scorer script added

---
+++ Assets/Scripts/Scorer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    private int hits = 0;
+
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log("You've bumped into a thing this many times: " + ++hits );    
+    }
+}
