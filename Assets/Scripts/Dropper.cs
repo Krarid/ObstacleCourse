30af6af47954a378c58e3eeb19203ed2120e9892
Javier Meléndez <javier.melendez@fyware.com>
2022-03-05 22:22:50 -0600

    [feature] Objects fall
    1. Using Time.time
    2. If statements
    3. Caching a reference

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    [SerializeField] private float timeToWait = 3.0f;
+    MeshRenderer meshRenderer = null;
+    Rigidbody rigidBody = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        rigidBody = GetComponent<Rigidbody>();
+
+        meshRenderer.enabled = false;
+        rigidBody.useGravity = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > timeToWait ) {
+            meshRenderer.enabled = true;
+            rigidBody.useGravity = true; 
+        }
+    }
+}
